916b1bc18609057e19b4b04d570aa15f14d16a92
Aubry TONNERRE <47123188+kiurow590@users.noreply.github.com>
2025-02-07 03:36:50 +0000

    init file project

---
+++ src/main/java/com/uqac/wifi_attack/WifiAttackApplication.java
@@ -0,0 +1,13 @@
+package com.uqac.wifi_attack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WifiAttackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WifiAttackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uqac/wifi_attack/WifiAttackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uqac.wifi_attack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WifiAttackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
